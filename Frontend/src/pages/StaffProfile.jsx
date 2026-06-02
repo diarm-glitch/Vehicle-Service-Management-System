c31bdbb7fb8ca9eab795a5f96cd103b98e8e1c0a
diarm-glitch <diar.mustafa24@gmail.com>
2026-06-02 06:33:15 +0200

    SraffProfile .jsx

---
+++ Frontend/src/pages/StaffProfile.jsx
@@ -0,0 +1,20 @@
+function StaffProfile() {
+  const userId = localStorage.getItem("userId");
+  const emri = localStorage.getItem("emri");
+  const mbiemri = localStorage.getItem("mbiemri");
+  const role = localStorage.getItem("role");
+
+  return (
+    <div className="container mt-4">
+      <h2>Staff Profile</h2>
+
+      <div className="card p-4">
+        <p><strong>ID:</strong> {userId}</p>
+        <p><strong>Name:</strong> {emri} {mbiemri}</p>
+        <p><strong>Role:</strong> {role}</p>
+      </div>
+    </div>
+  );
+}
+
+export default StaffProfile;
